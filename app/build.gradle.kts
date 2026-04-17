56e0f2200b80d15c364a8fc0a67503ef37273bc9
ashb155 <b.ashutosh198@gmail.com>
2026-04-17 12:32:30 +0530

    gradle update 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,5 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
-
-// DataStore
     implementation(libs.androidx.datastore.preferences)
 }
